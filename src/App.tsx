e6de3153862d4adf5b1cf595f0816ad8d48eb86c
bannerett <33360251+bannerett@users.noreply.github.com>
2024-02-28 00:27:39 +0100

    feat: set up score
    Closes ttt-7

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 
 import Game from '~/components/game/Game.tsx';
+import Score from '~/components/game/Score.tsx';
 import Navbar from '~/components/Navbar.tsx';
 
 function App() {
   return (
     <Box sx={{ fontSize: '1.25rem', height: '100svh' }}>
       <Navbar />
-      <Box component="main" sx={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Stack component="main" direction="column" height="100%" alignItems="center" justifyContent="center" rowGap={2}>
         <Game />
-      </Box>
+        <Score />
+      </Stack>
     </Box>
   );
 }
