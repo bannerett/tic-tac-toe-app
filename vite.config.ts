10b8c08f75e842a937a47ddaad5ab4cae5efe3c0
bannerett <33360251+bannerett@users.noreply.github.com>
2024-02-27 21:12:54 +0100

    fix: favicon icon sizes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ const manifest: Partial<VitePWAOptions> = {
     theme_color: '#000000',
     background_color: '#ffffff',
     icons: [
-      { src: '/favicon.ico', sizes: '192x192', type: 'image/png', purpose: 'any' },
+      { src: '/favicon.ico', sizes: '32x32 16x16', type: 'image/png', purpose: 'any' },
       { src: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
       { src: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
       {
