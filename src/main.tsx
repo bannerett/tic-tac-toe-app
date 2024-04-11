fb916cc184b25eec91ba7f21a25ce0f2f366929d
bannerett <33360251+bannerett@users.noreply.github.com>
2024-04-11 16:34:32 +0200

    feat: set up basic service worker (#9)
    Closes ttt-9

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import App from '~/App.tsx';
 import MuiProvider from '~/providers/MuiProvider.tsx';
 import { store } from '~/state/store.ts';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then((registration) => {
+    console.log('Service Worker registration', registration);
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
